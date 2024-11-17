78a7cd8dae97c525f7e11af7b095a257f3239437
Benja Martinez <utm23090635@utma.edu.mx>
2024-11-17 15:26:40 -0600

    creacion de front HTML y CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { AiFillCheckCircle, AiTwotoneDelete } from "react-icons/ai";
 
 function App() {
+  const [isCompletedScreen, setIsCompletedScreen] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MyTodolist</h1>
+
+      <div className="todo-wrapper">
+        <div className=" todo-input">
+          <div className="todo-input-item">
+            <label> Titulo </label>
+            <input type="text" placeholder=" Titulo de tu tarea?" />
+          </div>
+          <div className="todo-input-item">
+            <label> Descripcion</label>
+            <input type="text" placeholder=" Que quieres hacer?" />
+          </div>
+          <div className="todo-input-item">
+            <button type="button" className="primaryBtn">
+              Añadir
+            </button>
+          </div>
+        </div>
+
+        <div className="btn-area">
+          <div>
+            <button
+              className={`secondarybtn ${!isCompletedScreen ? "active" : ""}`}
+              onClick={() => setIsCompletedScreen(false)}
+            >
+              Por Hacer
+            </button>
+            <button
+              className={`secondarybtn ${isCompletedScreen ? "active" : ""}`}
+              onClick={() => setIsCompletedScreen(true)}
+            >
+              Completado
+            </button>
+          </div>
+        </div>
+
+        <div className="todo-list">
+          <div className="todo-list-item">
+            <h3>Tarea 1</h3>
+            <p>Descripcion</p>
+
+            <div>
+              <AiTwotoneDelete className="icons" />
+              <AiFillCheckCircle className="icons-check" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
